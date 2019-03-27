c6ac922804064ea0d54b301eab267c5a98ca3081
emma <emma.x.sullivan@gmail.com>
2019-03-27 15:02:46 +0000

    Emoji picker working

--- public/components/PrAddMessage.js
+++ public/components/PrAddMessage.js
@@ -1,17 +1,81 @@
 import React, { Component } from "react";
 import "../css/PrAddMessage.css";
-import EmojiModal from "./EmojiModal";
+import 'emoji-mart/css/emoji-mart.css'
+import { NimblePicker } from 'emoji-mart'
+import data from 'emoji-mart/data/emojione.json'
 
 class PrAddMessage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      showEmojis: false,
+    }
+  }
+
+  handleEmojiClick = (evt) => {
+    console.log(evt.unified)
+    let sym = evt.unified.split('-')
+    let codesArray = []
+    sym.forEach(el => codesArray.push('0x' + el))
+    let emojiPic = String.fromCodePoint(...codesArray)
+    this.setState((prevState) => {
+      return {text: prevState.text + emojiPic};
+    })
+  }
+
+  handleMessageChange = (evt) => {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
+  showEmojis = (e) => {
+    this.setState({
+      showEmojis: true
+    }, () => document.addEventListener('click', this.closeMenu))
+  }
+
+  closeMenu = (e) => {
+    console.log(this.emojiPicker)
+    if (this.emojiPicker !== null && !this.emojiPicker.contains(e.target)) {
+      this.setState({
+        showEmojis: false
+      }, () => document.removeEventListener('click', this.closeMenu))
+    }
+  }
+
   render() {
+    const {showEmojis} = this.state;
+    const {text} = this.state.text;
+
     return (
       <React.Fragment>
         <div id="new-message" className="chatWindow-footer">
           <div className="footer-container">
             <div className="input-box-container">
-              <EmojiModal />
+              <button type="button" className="emojiContainer" onClick={this.showEmojis}>
+                <i className="far fa-smile" />
+              </button>
+              {showEmojis && (
+                <div className="emojiPickerContainer" ref={el => (this.emojiPicker = el)}>
+                  <NimblePicker
+                    onSelect={this.handleEmojiClick}
+                    data={data}
+                    title="Pick your emoji…"
+                    emoji="point_up"
+                    set="emojione"
+                  />
+                </div>
+              )}
               <div className="input-flexContainer">
-                <input className="input-box" placeholder="Message Prospela..."/>
+                <form>
+                  <input
+                    className="input-box"
+                    placeholder="Type message..."
+                    type="text"
+                    value={this.state.text}
+                    onChange={this.handleMessageChange}
+                  />
+                </form>
                 <button type="button" className="attachmentContainer">
                   <i className="fas fa-paperclip" />
                 </button>
