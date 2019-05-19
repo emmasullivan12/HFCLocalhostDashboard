45a2af73f0de1d48f2f40b9da9c183033c8b72f4
emma <emma.x.sullivan@gmail.com>
2019-05-19 20:55:27 +0100

    verify email page

--- public/components/VerifyEmail.js
+++ public/components/VerifyEmail.js
@@ -5,36 +5,116 @@ import ReactDOM from "react-dom";
 import "../css/Login.css";
 
 class VerifyEmail extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      isContainerOpen: false,
+      verifCode: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleContainer = this.toggleContainer.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { verifCode } = this.state;
+    const submission = {
+      token: verifCode
+    };
+    console.log(submission);
+//    this.props.verifyEmail(submission);
+//    window.location.assign('https://test.prospela.com');
+  }
+
+  handleResendSubmit(e) {
+    e.preventDefault();
+    console.log("requesting to resend email verification code");
+//    this.props.resendVerifyEmail();
+  }
+
+  canBeSubmitted() {
+    const { verifCode } = this.state;
+    return (
+      verifCode.length > 0
+    );
+  }
+
+  toggleContainer() {
+    const currentState = this.state.isContainerOpen;
+    this.setState({ isContainerOpen: !currentState });
+  }
+
   render() {
+  const isEnabled = this.canBeSubmitted();
+  const {isContainerOpen, verifCode} = this.state;
+
     return (
       <React.Fragment>
-        <div className="prLogoArea col-12">
-          <div className="prLogoContainer col-12">
-            <img className="prLogoImg" alt="Prospela Logo" src="https://prospela.com/wp-content/uploads/2019/05/Prospela-New-Logo_Colour.png" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-7 col-s-12 centerContainer">
-            <div className="loginContainer">
-              <div>
-                <h1>Enter your code</h1>
-                <div className="header-descriptor">We&#39;ve sent an email verification code to your email. Please enter it below. Note: code only <span className="bold">valid for the next 24 hours.</span></div>
-              </div>
-              <form method="POST" action="/login">
-                <div className="form-group">
-                  <div className="descriptor">Enter email verification code</div>
-                  <input type="password" name="emailverfcode" className="form-control-pwd" required id="emailverifcode"/>
-                </div>
-                <button type="submit" className="btn-general button-unstyled" id="emailverif-btn">Verify Email</button>
-              </form>
-              <div className="descriptor">Can&#39;t see the email?</div>
-              <div className="login-error-msg">The email verification code does not exist. Please check and try again, or <a className="error-href" href="https://test.prospela.com/resendemailverificationcode">resend code</a></div>
-              <div className="legalSection">Copyright 2019 All Rights Reserved. Made with ♥ by Prospela Group Ltd <a className="legal-href" href="https://prospela.com/privacy-policy/">Privacy</a> | <a className="legal-href" href="https://prospela.com/terms-of-use-safeguarding-policy/">Terms and Safeguarding</a></div>
+        <div className="mainContainer">
+          <div className="prLogoArea col-12">
+            <div className="prLogoContainer col-12">
+              <img className="prLogoImg" alt="Prospela Logo" src="https://prospela.com/wp-content/uploads/2019/05/Prospela-New-Logo_Colour.png" />
             </div>
           </div>
-          <div className="col-5 login-pic">
-            <div className="login-picContainer">
-              <img className="prPlatformImg" alt="Prospela Platform Cartoon" src="https://prospela.com/wp-content/uploads/2019/05/Platform-picture.png" />
+          <div className="row">
+            <div className="col-7 col-s-12 centerContainer">
+              <div className="loginContainer">
+                <div>
+                  <h1>Hey USER NAME!</h1>
+                  <div className="header-descriptor">We&#39;ve sent an email verification code to USER EMAIL. Please enter it below. Note: code only <span className="bold">valid for the next 24 hours.</span></div>
+                </div>
+                <form onSubmit={this.handleSubmit}>
+                  <div className="form-group">
+                    <div className="descriptor">Enter email verification code</div>
+                    <input
+                      type="password"
+                      name="verificationCode"
+                      className="form-control-pwd"
+                      id="emailverifcode"
+                      value={this.state.verifCode}
+                      onChange={this.handleChange}
+                      required
+                      autoFocus
+                      minLength="6"
+                      maxLength="6"
+                    />
+                  </div>
+                  <button type="submit" disabled={!isEnabled} className="btn-general button-unstyled" id="emailverif-btn">
+                    Verify Email
+                  </button>
+                </form>
+                <div className="login-error-msg">The email verification code does not exist. Please check and try again, or <a className="error-href" href="https://test.prospela.com/resendemailverificationcode">try re-entering your email</a></div>
+                <button type="button" className="btnDescriptor button-unstyled alignLeft" onClick={this.toggleContainer}>
+                  <span >Can&#39;t see the email? </span>
+                  <span className="exclamation-icon-container">
+                    <i className="fas fa-question-circle" />
+                  </span>
+                </button>
+                {isContainerOpen && (
+                  <div>
+                    <ol>
+                      <li>Check your junk email folder</li>
+                      <li>Check you entered your email address correctly (USER EMAIL). If not, <a href="https://test.prospela.com/signup">try signing up again</a></li>
+                      <li>Wait a few minutes or alternatively click to resend below</li>
+                      <button type="submit" className="btn-general button-unstyled alignLeft" onClick={this.handleResendSubmit}>
+                        <span >Resend code</span>
+                      </button>
+                      <div> Something went wrong. Looks like this user has already been verified. Please try and log in</div>
+                      <div> Successfully resent. Please also check your junk mail.</div>
+                    </ol>
+                  </div>
+                )}
+                <div className="legalSection">Copyright 2019 All Rights Reserved. Made with ♥ by Prospela Group Ltd <a className="legal-href" href="https://prospela.com/privacy-policy/">Privacy</a> | <a className="legal-href" href="https://prospela.com/terms-of-use-safeguarding-policy/">Terms and Safeguarding</a></div>
+              </div>
+            </div>
+            <div className="col-5 login-pic">
+              <div className="login-picContainer">
+                <img className="prPlatformImg" alt="Prospela Platform Cartoon" src="https://prospela.com/wp-content/uploads/2019/05/Platform-picture.png" />
+              </div>
             </div>
           </div>
         </div>
