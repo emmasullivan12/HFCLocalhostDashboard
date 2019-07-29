32c2d2872ba81bf75065482d61ad29964d4560eb
emma <emma.x.sullivan@gmail.com>
2019-07-29 18:38:54 +0100

    1.0 Dex merged this code on 16th May 2019

--- public/InitiativeLandingPage.js
+++ public/InitiativeLandingPage.js
@@ -1,6 +1,12 @@
 /* Dex last merged this code on 19th May 2019*/
 
 var pwdByNav = document.getElementById("pwdByNav");
+var menteeClick = document.getElementById("mentee-click");
+var mentorClick = document.getElementById("mentor-click");
+var menteeBtn = document.getElementById("mentee-btn");
+var mentorBtn = document.getElementById("mentor-btn");
+var menteeInfo = document.getElementById("mentee-info");
+var mentorInfo = document.getElementById("mentor-info");
 
 window.onscroll = function () {
     if (window.pageYOffset >= 450 ) {
@@ -10,3 +16,17 @@ window.onscroll = function () {
       pwdByNav.classList.remove("is-fixed");
     }
 };
+
+// Add event listenres
+menteeClick.addEventListener('click', function(event) {
+  mentorInfo.classList.remove("active");
+  mentorBtn.classList.remove("active");
+  menteeInfo.classList.add("active");
+  menteeBtn.classList.add("active");
+});
+mentorClick.addEventListener('click', function(event) {
+  menteeInfo.classList.remove("active");
+  menteeBtn.classList.remove("active");
+  mentorInfo.classList.add("active");
+  mentorBtn.classList.add("active");
+});
