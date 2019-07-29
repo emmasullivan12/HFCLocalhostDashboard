86361f13c825d79555382d7a430ff9fcc277d7a0
emma <emma.x.sullivan@gmail.com>
2019-07-29 22:38:05 +0100

    1.0 Dex merged this code on 17th May 2019

--- public/InitiativeLandingPage.js
+++ public/InitiativeLandingPage.js
@@ -7,6 +7,9 @@ var menteeBtn = document.getElementById("mentee-btn");
 var mentorBtn = document.getElementById("mentor-btn");
 var menteeInfo = document.getElementById("mentee-info");
 var mentorInfo = document.getElementById("mentor-info");
+var stepsTitle = document.getElementById("stepsTitle");
+var menteeStepsList = document.getElementById("menteeStepsList");
+var mentorStepsList = document.getElementById("mentorStepsList");
 
 window.onscroll = function () {
     if (window.pageYOffset >= 450 ) {
@@ -21,12 +24,18 @@ window.onscroll = function () {
 menteeClick.addEventListener('click', function(event) {
   mentorInfo.classList.remove("active");
   mentorBtn.classList.remove("active");
+  mentorStepsList.classList.remove("active");
+  menteeStepsList.classList.add("active");
   menteeInfo.classList.add("active");
   menteeBtn.classList.add("active");
+  stepsTitle.innerHTML = 'Steps to joining as a Mentee';
 });
 mentorClick.addEventListener('click', function(event) {
   menteeInfo.classList.remove("active");
   menteeBtn.classList.remove("active");
+  menteeStepsList.classList.remove("active");
+  mentorStepsList.classList.add("active");
   mentorInfo.classList.add("active");
   mentorBtn.classList.add("active");
+  stepsTitle.innerHTML = 'Steps to joining as an E-Mentor';
 });
