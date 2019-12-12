d5d06debc07c836cf4687987c915b04d63e5f82b
emma <emma.x.sullivan@gmail.com>
2019-12-12 15:04:15 +0000

    1.0 Dex merged this code on 12th Dec 2019

--- public/signUp.js
+++ public/signUp.js
@@ -1,4 +1,4 @@
-/* Dex last merged this code on 14th Sept 2019*/
+/* Dex last merged this code on 12th Dec 2019*/
 var formControlStd = document.getElementsByClassName("form-control-std");
 
 var tncCheckbox = document.getElementById("tncCheckbox");
