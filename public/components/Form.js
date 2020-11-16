27eb64f28936855c31cb292d0a371dc73fcb5b59
Emma <emma.x.sullivan@gmail.com>
2020-11-16 12:52:03 +0000

    1.0 Merged this code 16th Nov 2020

--- public/components/Form.js
+++ public/components/Form.js
@@ -1,4 +1,4 @@
-// Dex last merged this code on 9th nov 2020
+// Dex last merged this code on 16th nov 2020
 
 import React, { Component } from "react";
 
@@ -215,7 +215,7 @@ class Form extends Component {
               qViewed: maxQViewed
             }
 
-          })        
+          })
         }
       });
     }, options);
