d58645258ce361d2fb120dde20766dade3df59fe
Emma <emma.x.sullivan@gmail.com>
2021-03-03 12:53:46 +0000

    Needs merging

---
+++ public/emailTemplate-newMenteeMatch-puretext.js
@@ -0,0 +1,18 @@
+// Merged on 12th oct 2020
+const ejs = require("ejs");
+
+const email = 'dexter@hotmail.com'
+
+ejs.renderFile(__dirname + "/emailTemplate-newMenteeMatch.ejs", function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    var mailOptions = {
+      from:  'Prospela',
+      to: email,
+      subject: 'You have a new Mentee match on Prospela!',
+      text: 'You have a new mentoring match on Prospela!\n\nOne of our team have scouted a new Mentee match for you 🎉 \n\nThey\'re particularly keen to speak with you and they\'ve already dropped you a little message \n\nClick below to check out their profile (which we\'ve sent you by DM) and accept to start chatting (or kindly decline if you don\'t think it\'s quite the right fit).\n\nSee your Match at \'https://app.prospela.com\'\n\nAs always, you can reach out to us if you need any help or ideas for how to introduce yourself. Chat to us in the platform =)\n\nFrom, The Team @ Prospela',
+      html: data
+    }
+  }
+});
---
+++ public/emailTemplate-newMentorMatch-puretext.js
@@ -0,0 +1,19 @@
+// Merged on 12th oct 2020
+const ejs = require("ejs");
+
+const email = 'dexter@hotmail.com'
+const mentorsGroup = 'Access:VFX'
+
+ejs.renderFile(__dirname + "/emailTemplate-newMentorMatch.ejs", {mentorsGroup: mentorsGroup}, function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    var mailOptions = {
+      from:  'Prospela',
+      to: email,
+      subject: 'You have a new ' + mentorsGroup + ' E-Mentor match on Prospela!',
+      text: 'You have a new mentoring match on Prospela!\n\nOne of our team have scouted an ' + mentorsGroup + ' E-Mentor match for you 🎉 \n\nClick below to check out the message we\'ve sent you (sharing your E-Mentor\'s profile) and accept to start chatting, but be quick! - they might not be available for long.\n\nSee your Match at \'https://app.prospela.com\'\n\nAs always, you can reach out to us if you need any help or ideas for how to introduce yourself. Chat to us in the platform =)\n\nFrom, The Team @ Prospela',
+      html: data
+    }
+  }
+});
