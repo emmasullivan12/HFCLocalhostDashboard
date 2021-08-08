859238220266ec8699cc3a0a671a95ac4e77a6ba
Emma <emma.x.sullivan@gmail.com>
2021-08-08 18:43:16 +0100

    1.0 Merged this code 8th Aug 2021

--- public/components/UserDetail.js
+++ public/components/UserDetail.js
@@ -1,4 +1,4 @@
-// Dex last merged this code on 19th july 2021
+// Dex last merged this code on 8th aug 2021
 
 import React, { Component } from "react";
 import subjectsOptions from './Subjects.js';
