9b6ba27e1ab0e3f1827b1405b211001199d08214
Emma <emma.x.sullivan@gmail.com>
2021-12-31 16:57:25 +0000

    1.0 Merged this code 31st Dec 2021

--- public/signUp.js
+++ public/signUp.js
@@ -1,4 +1,4 @@
-/* merged this code on 31st aug 2021 */
+/* merged this code on 31st dec 2021 */
 //import { personalEmails } from './PersonalEmails.js';
 
 var formControlStd = document.getElementsByClassName("form-control-std");
