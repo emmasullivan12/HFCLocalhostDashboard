842c77a614f728d5b54bf854ac8176f0c775a7c0
Emma <emma@prospela.com>
2023-04-20 14:32:43 +0100

    needs merging

--- public/components/HomePage.js
+++ public/components/HomePage.js
@@ -1115,13 +1115,15 @@ class HomePage extends Component {
     const {userStepsIsOpen, userstep, userRole, showSuccessModal, showAddSkillsModal, showAnswerAQModal, showAskAQModal, showMentorFullAppModal, showMenteeFullAppModal, showJoinAGroupModal, showMentorIDModal, showMentorCVModal, showMentorTrainingModal, showMenteeTrainingModal} = this.state;
   //  const groupName = 'AVFX' // If step is 'autoenroll' then show the groupname
   //  const hasJoinedAutoEnrollGroup = false
-    let expertise, learning, userHIDs, userQIDs, numUserQs, numUserAnswers, wantsU18, userGroups, hasMatch, mentorSteps, menteeSteps, isNonCoreCountry, isU18
+    let expertise, learning, userHIDs, userQIDs, numUserQs, numUserAnswers, wantsU18, userGroups, hasMatch, mentorSteps, menteeSteps, isNonCoreCountry, isFromO18OnlyCountry, isU18
     let country = 'GBR'
     learning = []
     userGroups = ['123']
   //  userGroups = []
     hasMatch = false
-    isNonCoreCountry = (country != 'GBR' && country != 'USA' && country != 'CAN' && country != 'NZL' && country != 'AUS')
+    isNonCoreCountry = (country != 'GBR' && country != 'USA' && country != 'CAN' && country != 'NZL' && country != 'AUS' && country != 'NLD' && country != 'DEU' && country != 'ESP' && country != 'FRA' && country != 'ITA' && country != 'BEL' && country != 'DNK' && country != 'SWE')
+    isFromO18OnlyCountry = (country == 'NZL' || country == 'AUS' || country == 'NLD' || country == 'DEU' || country == 'ESP' || country == 'FRA' || country == 'ITA' || country == 'BEL' || country == 'DNK' || country == 'SWE')
+    isu18 = true
 
     if (userRole == 'mentor') {
       expertise = []
@@ -1153,7 +1155,7 @@ class HomePage extends Component {
         {stepText: 'Visit your feed', modalToShow: '', isComplete: 1, validSteps: ['didEduEmailVerif', 'didReviewVerif']},
         {stepText: 'Add skills you want to learn', modalToShow: 'AddSkills', isComplete: learning && learning.length > 0, validSteps: ['didEduEmailVerif', 'didReviewVerif'], requireLogin: true},
         {stepText: 'Ask a question', modalToShow: 'AskAQ', isComplete: numUserQs && numUserQs > 0, validSteps: ['didShortSUtf'], requireLogin: true},
-        {stepText: 'Join a mentoring programme', modalToShow: 'JoinAGroup', isComplete: userGroups && userGroups.length > 0, validSteps: ['didShortSUtf'], requireLogin: true, limitForNonCoreCountries: true, tooltiptextWhenLocked: 'Mentoring programmes are not available in your country yet', },
+        {stepText: 'Join a mentoring programme', modalToShow: 'JoinAGroup', isComplete: userGroups && userGroups.length > 0, validSteps: ['didShortSUtf'], requireLogin: true, O18CountriesOnly: true, limitForNonCoreCountries: true, tooltiptextWhenLocked: (isNonCoreCountry == true ? 'Mentoring programmes are not available in your country yet' : ((isFromO18OnlyCountry == true && isU18 == true) ? 'Mentoring for under 18s is not available in your country yet' : 'Not yet available')), },
         {stepText: 'Complete your full mentee application', modalToShow: 'MenteeFullApp', isComplete: (userstep == 'didFullSUtf' || userstep == 'didSafeG'), reqStep: 'JoinAGroup', tooltiptextWhenLocked: 'Join a mentoring programme to unlock this step', validSteps: ['didShortSUtf']},
         {stepText: 'Complete your 5-min mentee training', modalToShow: 'MenteeTraining', isComplete: userstep == 'didSafeG', reqStep: 'MenteeFullApp', tooltiptextWhenLocked: 'Complete your full mentee application to unlock this step', validSteps: ['didFullSUtf']},
       ]
@@ -1223,7 +1225,7 @@ class HomePage extends Component {
             <React.Fragment>
               <div className="marginTop10">
                 {steps.map((step, index) => {
-                  const reqStepsComplete = (step.limitForNonCoreCountries != null && step.limitForNonCoreCountries == true && isNonCoreCountry == true) ? false : (step.reqStep != null ? steps.filter(x => x.modalToShow == step.reqStep)[0].isComplete : true)
+                  const reqStepsComplete = ((step.limitForNonCoreCountries != null && step.limitForNonCoreCountries == true && isNonCoreCountry == true) || (step.O18CountriesOnly != null && step.O18CountriesOnly == true && isFromO18OnlyCountry == true)) ? false : (step.reqStep != null ? steps.filter(x => x.modalToShow == step.reqStep)[0].isComplete : true)
                   return (
                     <div key={index} onClick={() => this.showModal(step.isComplete, reqStepsComplete, step.modalToShow, step.requireLogin)} className={reqStepsComplete != true ? "tooltip" : ""}>
                       <Checkbox
