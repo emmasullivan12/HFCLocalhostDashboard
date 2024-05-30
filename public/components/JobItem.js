a0bce6c8079f637ab4d695c5466bc4d5e2850c4e
emmasullivan12 <emma.x.sullivan@gmail.com>
2024-05-30 15:49:02 +0100

    1.0 Merged this code 30th May2024

--- public/components/JobItem.js
+++ public/components/JobItem.js
@@ -1,4 +1,4 @@
-// Dex last merged this code on 30th may 2024
+// Dex last merged this code on 30th  may 2024
 
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
